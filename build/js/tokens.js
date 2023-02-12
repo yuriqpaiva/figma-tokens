649294470843ab5b92dd8115ab17a36f33f1cb17
Yuri <yuriqpaiva@gmail.com>
2023-02-12 10:48:59 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 12 Feb 2023 13:44:14 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#141414",
+  "ColorPurple500": "#8257ec"
+};
