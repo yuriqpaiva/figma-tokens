239e8228943b7d543635228326e7136e2990b873
yuriqpaiva <yuriqpaiva@users.noreply.github.com>
2023-02-12 14:45:50 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Sun, 12 Feb 2023 13:44:14 GMT
+ * Generated on Sun, 12 Feb 2023 14:45:49 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#141414",
+  "ColorPurple400": "#a07ff0",
   "ColorPurple500": "#8257ec"
 };
